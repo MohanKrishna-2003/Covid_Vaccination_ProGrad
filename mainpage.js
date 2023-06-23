8ce01a50175d1be1e9a51932d063769743ce394e
MohanKrishna-2003 <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-06-23 08:45:05 +0530

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#element', {
+  strings: ['PREVENTION IS', 'BETTER THAN CURE.'],
+  typeSpeed: 60,
+  loop: true
+});
+var typed = new Typed('#elements', {
+  strings: ['Fight Virus With Vaccines!', 'Do Vaccination Today!', 'Be Healthy Tomorrow!'],
+  typeSpeed: 60,
+  loop: true
+});
+var typed = new Typed('#ele', {
+  strings: ['Why should I vaccine?'],
+  typeSpeed: 60,
+  loop: true
+});
